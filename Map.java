736e42c36cccc6e40ce3304d26f9c45e4b5f99e7
luke roche <lukerochedev@gmail.com>
2018-02-15 13:52:45 -0500

    initial setup boi

---
+++ Map.java
@@ -0,0 +1,23 @@
+
+
+
+public class Map{
+
+	public enum MapFlags{OPEN, VISITED, BLOCKED, START, FINISH}
+
+	private Vector2 dimensions;
+	private MapFlags map[][];
+	
+	public Map(int width, int height){
+		dimensions = new Vector2(width, height);
+		map = new MapFlags[dimensions.x][dimensions.y];
+		
+	}
+
+	public void setBlock(int x, int y, MapFlags flag){
+		map[x][y] = flag;
+	}
+
+	public MapFlags getMapInfo(int x, int y){ return map[x][y]; }
+
+}
